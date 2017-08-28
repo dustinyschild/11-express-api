17483be5e8cfe93fac538f7602f8aae439aea7ec
Dustin <dyelland@gmail.com>
2017-08-28 13:37:38 -0500

    played around with the new npm packages and set up some 404s

--- lab-dustin/server.js
+++ lab-dustin/server.js
@@ -1,14 +1,30 @@
 'use strict';
 
 const PORT = process.env.PORT;
+const debug = require('debug')('http');
 const createError = require('http-errors');
 const express = require('express');
 const app = express();
 
 app.get('/',function(req,res){
-  res.send('Hello World');
+  debug('Homepage');
+  res.end();
+});
+
+app.get('/notes',function(req,res){
+  debug('getting notes');
+  res.send('your notes');
+});
+
+//requests catch all
+app.get('*',(req,res) => {
+  res.send(createError(404, 'Not Found'));
+});
+
+app.post('*',(req,res) => {
+  res.send(createError(404, 'Not Found'));
 });
 
 app.listen(PORT,function(){
-  console.log(`Listening on Port ${PORT}`)
+  debug(`Listening on port ${PORT}`);
 });
